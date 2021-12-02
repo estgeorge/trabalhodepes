655da135e8afbea0b5a93408bec34a19903a4896
estgeorge <39488994+estgeorge@users.noreply.github.com>
2021-12-01 23:36:34 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 
-
-# create the application object
 app = Flask(__name__)
-
-# use decorators to link the function to a url
+app.secret_key = 'asdkjv99jkkHGks34'
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')  # render a template
+    return render_template('home.html')
 
 
-# Route for handling the login page logic
+@app.route('/areadocliente')
+def areadocliente():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
+    username = session['username']
+    return 'Logged in as ' + username + '<br>' + \
+        "<b><a href = '/logout'>click here to log out</a></b>"
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -21,8 +27,14 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Senha inválida. Tente novamente.'
         else:
-            return redirect(url_for('home'))
+            session['username'] = request.form['username']
+            return redirect(url_for('areadocliente'))
     return render_template('login.html', error=error)
+
+
+def logged_in():
+    if 'username' not in session:
+        return redirect(url_for('login'))
 
 
 # start the server with the 'run()' method
