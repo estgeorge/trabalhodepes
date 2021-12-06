962f64c63f59bb8ec51b8fd6e8dd0b2b14b54280
estgeorge <39488994+estgeorge@users.noreply.github.com>
2021-12-06 18:02:47 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,28 +7,44 @@ app = Flask(__name__)
 app.secret_key = b'\xf85~.("^\xee\r\xf20OF\xbaC\xff'
 
 
-def logged_out():
-    return 'email' not in session
-
-
-app.jinja_env.globals.update(logged_out=logged_out)
-app.jinja_env.globals.update(number2real=number2real)
-
-
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 
+def get_produto(produto_id):
+    conn = get_db_connection()
+    sql = f'SELECT * FROM estoque WHERE id = {produto_id}'
+    produto = conn.execute(sql).fetchone()
+    conn.close()
+    if produto is None:
+        abort(404)
+    return produto
+
+
+def carrinho_itens():
+    if 'pedidos' in session:
+        pedidos = session['pedidos']
+        return len(pedidos)
+    else:
+        return 0
+
+
+def is_logged_out():
+    return 'email' not in session
+
+
 @app.route('/')
 def home():
+    if is_logged_out():
+        session.pop('pedidos', None)
     return render_template('home.html')
 
 
 @app.route('/produtos')
 def produtos():
-    if logged_out():
+    if is_logged_out():
         return redirect(url_for('login'))
 
     produtos = None
@@ -45,26 +61,45 @@ def produtos():
     return render_template('produtos.html', produtos=produtos, resultado=resultado)
 
 
-def get_produto(produto_id):
-    conn = get_db_connection()
-    sql = f'SELECT * FROM estoque WHERE id = {produto_id}'
-    produto = conn.execute(sql).fetchone()
-    conn.close()
-    if produto is None:
-        abort(404)
-    return produto
-
-
 @app.route('/produto/<int:produto_id>')
 def produto(produto_id):
-    if logged_out():
+    if is_logged_out():
         return redirect(url_for('login'))
 
     produto = get_produto(produto_id)
     return render_template('produto.html', produto=produto)
 
 
-@app.route('/addusuario', methods=['GET', 'POST'])
+@app.route('/carrinho', methods=['GET', 'POST'])
+def carrinho():
+    if is_logged_out():
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        task = request.form['task']
+        if (task == "add"):
+            produto_id = int(request.form['produto_id'])
+            produto = get_produto(produto_id)
+            if produto:
+                if 'pedidos' in session:
+                    pedidos = session['pedidos']
+                else:
+                    pedidos = []
+                session['pedidos'] = add_pedido(pedidos, produto)
+        elif (task == "mod"):
+            pedido_id = int(request.form['pedido_id'])
+            nitens = int(request.form[f'nitens{pedido_id}'])
+            session['pedidos'] = update_pedido(
+                session['pedidos'], pedido_id, nitens)
+
+    if carrinho_itens() == 0:
+        return render_template('produtos.html')
+    else:
+        pedidos = session['pedidos']
+        return render_template('carrinho.html', pedidos=pedidos, total_geral=get_total_pedidos(pedidos))
+
+
+@ app.route('/addusuario', methods=['GET', 'POST'])
 def addusuario():
     if request.method == 'POST':
         nome = request.form['nome']
@@ -73,19 +108,20 @@ def addusuario():
         email = request.form['email']
         senha = request.form['senha']
         if not nome:
-            flash('O nome é requerido!')
+            flash('O nome é requerido!', 'danger')
         else:
             conn = get_db_connection()
             conn.execute('INSERT INTO usuario (nome, endereco, celular, email, senha) VALUES (?, ?, ?, ?, ?)',
                          (nome, endereco, celular, email, senha))
             conn.commit()
             conn.close()
+            flash('Usuário cadastrado com sucesso!', 'success')
             return redirect(url_for('home'))
 
     return render_template('cadusuario.html')
 
 
-@app.route('/addfarma', methods=['GET', 'POST'])
+@ app.route('/addfarma', methods=['GET', 'POST'])
 def addfarma():
     if request.method == 'POST':
         nome = request.form['nome']
@@ -94,19 +130,20 @@ def addfarma():
         email = request.form['email']
         senha = request.form['senha']
         if not nome:
-            flash('O nome é requerido!')
+            flash('O nome é requerido!', 'danger')
         else:
             conn = get_db_connection()
             conn.execute('INSERT INTO farmacia (nome, endereco, cnpj, email, senha) VALUES (?, ?, ?, ?, ?)',
                          (nome, endereco, cnpj, email, senha))
             conn.commit()
             conn.close()
+            flash('Farmácia cadastrada com sucesso!', 'success')
             return redirect(url_for('home'))
 
     return render_template('cadfarma.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@ app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -127,11 +164,16 @@ def login():
     return render_template('login.html', error=error)
 
 
-@app.route('/logout')
+@ app.route('/logout')
 def logout():
     session.pop('email', None)
+    session.pop('pedidos', None)
     return redirect(url_for('home'))
 
+
+app.jinja_env.globals.update(is_logged_out=is_logged_out)
+app.jinja_env.globals.update(number2real=number2real)
+app.jinja_env.globals.update(carrinho_itens=carrinho_itens)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,2 +1,59 @@
+def dumpclean(obj):
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            if hasattr(v, '__iter__'):
+                print(k)
+                dumpclean(v)
+            else:
+                print('%s : %s' % (k, v))
+    elif isinstance(obj, list):
+        for v in obj:
+            if hasattr(v, '__iter__'):
+                dumpclean(v)
+            else:
+                print(v)
+    else:
+        print(obj)
+
+
 def number2real(n):
     return f'{n:.2f}'.replace('.', ',')
+
+
+def build_pedidos(pedidos):
+    i = 0
+    pedidos_new = []
+    for pedido in pedidos:
+        if int(pedido['nitens']) > 0:
+            pedidos_new.append({'pedido_id': i,
+                                'produto': pedido['produto'],
+                                'preco': float(pedido['preco']),
+                                'id': int(pedido['id']),
+                                'nitens': int(pedido['nitens']),
+                                'total': int(pedido['nitens']) * float(pedido['preco'])
+                                })
+            i = i+1
+    return pedidos_new
+
+
+def get_total_pedidos(pedidos):
+    s = 0
+    for pedido in pedidos:
+        s = s+pedido['nitens']*pedido['preco']
+    return s
+
+
+def add_pedido(pedidos, produto):
+    pedidos.append({'produto': produto['produto'],
+                    'preco': produto['preco'],
+                    'id': produto['id'],
+                    'nitens': 1})
+    return build_pedidos(pedidos)
+
+
+def update_pedido(pedidos, pedido_id, nitens):
+    for pedido in pedidos:
+        if (int(pedido['pedido_id']) == int(pedido_id)):
+            pedido['nitens'] = nitens
+            break
+    return build_pedidos(pedidos)
